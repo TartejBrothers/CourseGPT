39b4010e56714bde1fdbcdea9cf3dc305dee2652
Taranjeet Singh Bedi <taranjeetsinghbedi2@gmail.com>
2025-04-09 15:30:07 +0530

    Added: Lesson Data

--- backend/routes/ai.js
+++ backend/routes/ai.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const generateLesson = require("../services/generateLesson");
-
+const Lesson = require("../models/ai");
 const router = express.Router();
+const verifyToken = require("../middlewares/auth");
 
 router.post("/lesson", async (req, res) => {
-  const { topic } = req.body;
+  const { topic, userId } = req.body;
+
   try {
     const lesson = await generateLesson(topic);
+
+    const match = lesson.match(/<h1[^>]*>(.*?)<\/h1>/i);
+    const heading = match ? match[1] : topic;
+
+    const lessonData = {
+      lessonData: lesson,
+      lessonHeading: heading,
+      userId: userId,
+    };
+    const lessonEntry = new Lesson(lessonData);
+    await lessonEntry.save();
     res.status(200).json({ lessonPlan: lesson });
   } catch (err) {
     console.error("Error generating lesson:", err);
